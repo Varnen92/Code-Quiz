7765a029ea7f82979fc918afed02431f4d7fb740
Robert Sandoval <robsand1992@yahoo.com>
2021-10-18 12:43:26 -0700

    added show question and show answers functions

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,29 +3,64 @@ var score = 0;
 const startButton = document.getElementById('start')
 const body = document.getElementById('mainbody')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
+let currentQuestionIndex
 
-
-let questions = [
+const questions = [
     {
-        
+        question: 'When was JavaScript introduced?',
+        answers: [
+            {text: '1990', correct: false},
+            {text: '1995', correct: true},
+            {text: '1997', correct: false},
+            {text: '1998', correct: false}
+
+        ]
     }
 ]
 
-/* var hideText = function() {
-    document.getElementById('mainbody').style.visibility = 'hidden';
-}
- */
-
 function startGame() {
     body.classList.add('hide')
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
-
+    setNextQuestion()
 }
+
+function setNextQuestion () {
+    resetState()
+    showQuestion(questions[currentQuestionIndex])
+}
+
+function resetState() {
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+
+    }
+}
+
+function showQuestion(question){
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
 
 function selectAnswer(){
 
 }
+
+
 
 
 var countdownTimer = function(){
